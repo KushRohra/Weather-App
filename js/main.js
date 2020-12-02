2578f9567067b2c090836df8d5a0996a08b6bc3e
KushRohra <iec2017024@iiita.ac.in>
2020-12-02 19:51:50 +0530

    City and main weather displayed

--- js/main.js
+++ js/main.js
@@ -10,21 +10,22 @@ function showPosition(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    function weatherBalloon( cityID ) {
+    function weatherBalloon() {
         var key = '52272358b1bbd36f9820c327ca4fe861';
         fetch('https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=' + key)  
         .then(function(resp) { return resp.json() }) // Convert data to json
         .then(function(data) {
           console.log(data);
-          displayData();
+          displayData(data);
         })
         .catch(function() {
           // catch any errors
         });
     }
-    weatherBalloon(2643743)
+    weatherBalloon();
 }
 
 function displayData(data) {
-    console.log("Data being displayed")
+    document.getElementById("city").innerHTML = data.name
+    document.getElementById("mainWeather").innerHTML = '<h5>'+data.weather[0].main+'</h5>';
 }
